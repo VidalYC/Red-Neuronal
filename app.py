cf07ddb9d938e9337dc511ec259e81b9b1b036bf
Yoriel <yorielvidal@gmail.com>
2024-05-01 10:50:32 -0500

    Save

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 import pandas as pd
+import numpy as np
+import json
+
 
 
 app = Flask(__name__)
@@ -13,7 +16,12 @@ def cargar_archivo_excel(ruta):
         print("Error al cargar el archivo:", e)
         return None
 
+global num_entradas
+global num_salidas
+global num_patrones
+
 def contar_elementos(matriz):
+    global num_entradas, num_salidas, num_patrones
     matriz_lista = matriz.tolist()  
     num_entradas = sum(row.count('x') for row in matriz_lista)
     num_salidas = sum(row.count('s') for row in matriz_lista)
@@ -62,6 +70,7 @@ def exito():
 def guardar_datos():
     if request.method == "POST":
         # Obtener los datos del formulario
+        
         num_capas_ocultas = int(request.form["capas"])
         neuronas_capa1 = int(request.form["neuronas1"])
         activacion_capa1 = request.form["activacion1"]
@@ -72,6 +81,26 @@ def guardar_datos():
         activacion_salida = request.form["activacionSalida"] if "activacionSalida" in request.form else None
         neuronas_salida = int(request.form["nSalida"]) if "nSalida" in request.form else None
 
+        # Generar las matrices según las condiciones especificadas
+        matrices = []
+
+        if num_capas_ocultas == 1:
+            WEC1 = np.random.uniform(-1, 1, (num_entradas, neuronas_capa1)).round(1).tolist()
+            WC1CS = np.random.uniform(-1, 1, (neuronas_capa1, num_salidas)).round(1).tolist()
+            matrices.extend([WEC1, WC1CS])
+        elif num_capas_ocultas == 2:
+            WEC1 = np.random.uniform(-1, 1, (num_entradas, neuronas_capa1)).round(1).tolist()
+            WC1C2 = np.random.uniform(-1, 1, (neuronas_capa1, neuronas_capa2)).round(1).tolist()
+            WC2CS = np.random.uniform(-1, 1, (neuronas_capa2, num_salidas)).round(1).tolist()
+            matrices.extend([WEC1, WC1C2, WC2CS])
+        elif num_capas_ocultas == 3:
+            WEC1 = np.random.uniform(-1, 1, (num_entradas, neuronas_capa1)).round(1).tolist()
+            WC1C2 = np.random.uniform(-1, 1, (neuronas_capa1, neuronas_capa2)).round(1).tolist()
+            WC2C3 = np.random.uniform(-1, 1, (neuronas_capa2, neuronas_capa3)).round(1).tolist()
+            WC3CS = np.random.uniform(-1, 1, (neuronas_capa3, num_salidas)).round(1).tolist()
+            matrices.extend([WEC1, WC1C2, WC2C3, WC3CS])
+
+
         # Guardar los datos en un archivo de texto
         with open("datos.txt", "w") as file:
             file.write(f"Número de capas ocultas: {num_capas_ocultas}\n")
@@ -81,9 +110,25 @@ def guardar_datos():
             if neuronas_capa3 is not None:
                 file.write(f"Número de neuronas para la capa 3: {neuronas_capa3}, Activación: {activacion_capa3}\n")
             file.write(f"Número de salidas para la capa salida: {neuronas_salida}, Activación: {activacion_salida}\n")
+            file.write(f"MATRIZ {matrices}\n")
 
-        return "Datos guardados exitosamente en datos.txt"
 
+
+        # Retorna los datos guardados
+        return jsonify({
+            "num_capas_ocultas": num_capas_ocultas,
+            "neuronas_capa1": neuronas_capa1,
+            "activacion_capa1": activacion_capa1,
+            "neuronas_capa2": neuronas_capa2,
+            "activacion_capa2": activacion_capa2,
+            "neuronas_capa3": neuronas_capa3,
+            "activacion_capa3": activacion_capa3,
+            "activacion_salida": activacion_salida,
+            "neuronas_salida": neuronas_salida,
+            "matrices": matrices
+            
+        })
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
